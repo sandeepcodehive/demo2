b454f75b44ad90cc3274d9e4a2e04861678cf1c5
Sandy712 <sandeepprajapat200@gmail.com>
2024-04-02 17:51:43 +0530

    done change

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -16,28 +16,32 @@ function Sidebar() {
 
     // console.log("after", isLoading);
 
-
-    if (!isAuthenticated) {
-        return null;
-    }
-
     return (
         <>
-            < li className="nav-item" >
-                <Link className="nav-link" to="/home" style={{ color: "white" }}>Chat</Link>
-            </li >
-            <li className="nav-item">
-                <Link className="nav-link" to="/dataset" style={{ color: "white" }}>Datasets</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/setting" style={{ color: "white" }}>Setting</Link>
-            </li>
-            <li className="nav-item">
-                <Link className="nav-link" to="/profile" style={{ color: "white" }}>Profile</Link>
-            </li>
-            <li className="nav-item">
-                <button className='nav-link' onClick={() => logout()} style={{ color: "white" }}>Log out</button>
-            </li>
+            {
+                isAuthenticated ?
+                    <>
+
+                        < li className="nav-item" >
+                            <Link className="nav-link" to="/home" style={{ color: "white" }}>Chat</Link>
+                        </li >
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/dataset" style={{ color: "white" }}>Datasets</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/setting" style={{ color: "white" }}>Setting</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/profile" style={{ color: "white" }}>Profile</Link>
+                        </li>
+                        <li className="nav-item">
+                            <button className='nav-link' onClick={() => logout()} style={{ color: "white" }}>Log out</button>
+                        </li>
+                    </>
+                    :
+                    null
+
+            }
         </>
     )
 }
